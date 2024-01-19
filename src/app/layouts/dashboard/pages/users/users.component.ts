e5c8c58ad0f4a6e0b918604d3a6c1b0a43a4da5a
MARCO ANTONIO MENDOZA HUILLCA <mmendozah@uandina.edu.pe>
2024-01-19 15:00:54 -0500

    user add

--- src/app/layouts/dashboard/pages/users/users.component.ts
+++ src/app/layouts/dashboard/pages/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from './models';
+
+
 
 @Component({
   selector: 'app-users',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
-
+  // displayedColumns: string[] = ['id','FullName'];
+  displayedColumns: string[] = ['id','fullName','email','role'];
+  dataSource: User[] = [
+    {
+      id: 1,
+      firstName: 'Naruto',
+      lastName: 'Uzumaqui',
+      email: 'naru@mail.com',
+      password: '123456',
+      role: 'ADMIN'
+    },
+    {
+      id: 2,
+      firstName: 'Sasuke',
+      lastName: 'Uchija',
+      email: 'sasu@mail.com',
+      password: '123456',
+      role: 'USER'
+    }
+  ];
 }
